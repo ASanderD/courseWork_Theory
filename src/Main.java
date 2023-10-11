a37d84f3c95a2931bc73f42f2bfca31c5b7f5cc6
ASanderD <Stepmini1989@gmail.com>
2023-10-11 22:32:01 +0700

    Task is done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str1 = "Мама мыла раму!!!";
+        String str2 = str1.substring(0, str1.length() / 2);
+        String str3 = str1.substring(str1.length() / 2);
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+        System.out.println(str1.length());
+        System.out.println(str2.length());
+        System.out.println(str3.length());
     }
 }
+
